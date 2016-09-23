147a1b509e0a693b9aea51f82d5430cd2fd4cc5e
WouterKompier <wouterjckompier@gmail.com>
2016-09-23 09:27:47 +0200

    device routings

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
-  skip_before_action :authenticate_user!, :except => [:home, :articles]
+  skip_before_action :authenticate_user!, :except => [:home]
+  # authorize! :edit, @article
   def index
     @articles = Article.all
   end
